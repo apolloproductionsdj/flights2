38767bebc012a77c1aa6433d534b6d9360d4caa3
mike <apolloproductionsdj@gmail.com>
2021-02-04 22:54:49 -0700

    created <App /> to project and deleted boiler plate code and replaced with h1

---
+++ app/javascript/components/App.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+// const App = () => {
+//     return (<div>Hello World</div>)
+// }
+
+// export default App;
+
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default App
